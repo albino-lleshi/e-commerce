f8acefe02e5f08e39e8946496ef63f31a3273f9d
albino-lleshi <101180824+albino-lleshi@users.noreply.github.com>
2022-10-09 23:23:33 +0200

    firebase and login page finished-2

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -9,11 +9,13 @@ import "./button.styles.scss";
 const BUTTON_TYPE_CLASSES = {
   google: "google-sign-in",
   inverted: "inverted",
-  default: "default",
 };
 const Button = ({ children, buttonType, ...otherProps }) => {
   return (
-    <button className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}>
+    <button
+      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
       {children}
     </button>
   );
